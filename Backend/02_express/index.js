cf06083126b0dff94445ae19b75f4039cdca7836
Paresh Kadve <kadveparesh03@gmail.com>
2025-06-28 00:11:05 +0530

    add git ginore and dependencies

--- Backend/02_express/index.js
+++ Backend/02_express/index.js
@@ -1,7 +1,9 @@
+import 'dotenv/config'
 import express from 'express';
 
 const app = express ()
 const port = 3000
+app.use(express.json())
 
 // app.get("/",(req,res)=>{
 //     res.send("hello from paresh kadve")
