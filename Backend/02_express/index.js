adf45d18f61ebb9500ac8ec17e22707a175f33ca
Paresh Kadve <kadveparesh03@gmail.com>
2025-06-28 01:16:49 +0530

    logger with morgan and winston

--- Backend/02_express/index.js
+++ Backend/02_express/index.js
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import express from 'express';
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express ()
 const port = 3000
@@ -19,13 +21,30 @@ app.use(express.json())
 
 app.use(express.json())
 
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 let teaData = []
 let nextId= 1
 
 
 // add a new data
 app.post('/teas',(req,res)=>{
-    
     const {name,price}=req.body
     const newTea= {id: nextId++ ,name,price}
     teaData.push(newTea)
@@ -45,7 +64,7 @@ app.get('/teas:id',(req,res)=>{
 })
 
 // update Tea  
-
+// update 
 app.put ('/teas/:id', (req,res)=>{
     
     const tea=teaData.find(t=> t.id=== parseInt(req.params.id))
