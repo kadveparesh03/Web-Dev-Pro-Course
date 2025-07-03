e191454190aed6c4b23f4114eeaa50fe04a523d0
Paresh Kadve <kadveparesh03@gmail.com>
2025-07-03 23:09:31 +0530

    update

--- vidtube/src/app.js
+++ vidtube/src/app.js
@@ -36,6 +36,7 @@
 import express from "express";
 import cors from "cors";
 import healthcheckRouter from "./routes/healthCheck.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
